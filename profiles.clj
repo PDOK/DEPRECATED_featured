68c0b42a782bffb307a32baa06fac985847ff067
Raymond Kroon <raymond@k3n.nl>
2015-04-02 22:55:38 +0200

    profiles.clj in gitignore for now

--- profiles.clj
+++
@@ -1,8 +0,0 @@
-{:dev  {:env {:processor-database-url "//localhost:5432/pdok"
-              :processor-database-user "pdok_owner"
-              :processor-database-password "pdok_owner"
-              :data-database-url "//localhost:5432/pdok"
-              :data-database-user "pdok_owner"
-              :data-database-password "pdok_owner"
-              }}
- :test {:env {:database-user "test-user"}}}
