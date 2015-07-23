2e46c79ecf58a343ecd580a7608756bb1de54417
Raymond Kroon <raymond@k3n.nl>
2015-07-23 12:13:21 +0200

    [PDOK-1584] double .whoops

--- src/main/clojure/pdok/postgres.clj
+++ src/main/clojure/pdok/postgres.clj
@@ -38,6 +38,9 @@
 (register-transit-write-handler org.joda.time.DateTime joda-time-writer)
 (register-transit-read-handler "m" joda-time-reader)
 
+(register-transit-write-handler org.joda.time.LocalDateTime joda-time-writer)
+(register-transit-read-handler "m" joda-time-reader)
+
 (register-transit-write-handler org.joda.time.LocalDate joda-local-date-writer)
 (register-transit-read-handler "ld" joda-local-date-reader)
 
@@ -129,6 +132,7 @@
     nil "text"
     clojure.lang.Keyword "text"
     clojure.lang.IPersistentMap "text"
+    org.joda.time.DateTime "timestamp without time zone"
     org.joda.time.DateTime "timestamp without time zone"
     org.joda.time.LocalDate "date"
     java.lang.Integer "integer"
