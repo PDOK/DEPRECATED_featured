aeca4de32dc1a562d54c774c2e75aad92391c53f
Jurgen te Stroet <jurgen.testroet@kadaster.nl>
2015-08-21 14:44:45 +0200

    PDOK-1650 proxy

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+export http_proxy=http://www-proxy.cs.kadaster.nl:8082
+export https_proxy=http://www-proxy.cs.kadaster.nl
+export no_proxy="localhost|127.0.0.1|*.SO"
 lein build && ./make-dar.sh
