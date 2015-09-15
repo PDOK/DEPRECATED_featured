b186afc1c315fd4de01316ca2825e976e14f76a0
Raymond Kroon <raymond.kroon@kadaster.nl>
2015-09-15 17:48:52 +0200

    fix: use DB functions for ...
    creating extractmanagement. Not because it is better, just because that is what we do now. With extra advantage that we can make use of the cleanup functions. We insert in the main table (without feature_type addition).

--- src/main/clojure/pdok/featured/extracts.clj
+++ src/main/clojure/pdok/featured/extracts.clj
@@ -31,46 +31,36 @@
 
 (defn features-for-extract [dataset feature-type extract-type features templates-dir]
   "Returns the rendered representation of the collection of features for a given feature-type inclusive tiles-set"
-  (if (empty? features) 
+  (if (empty? features)
     [nil nil]
     (let [template (template templates-dir dataset feature-type extract-type)
           partials (partials templates-dir dataset extract-type)]
       (if (or (nil? template) (nil? partials))
-        [(str "Template or partials cannot be found for dataset: " dataset 
-                                                    " feature-type: " feature-type 
-                                                    " extract-type: " extract-type 
+        [(str "Template or partials cannot be found for dataset: " dataset
+                                                    " feature-type: " feature-type
+                                                    " extract-type: " extract-type
                                                     " template-dir: " templates-dir) nil]
-        [nil (map #(vector (:_tiles %) (m/render template % partials) (:_valid_from %) (:_valid_to %)) features)]))))
-
-
-(defn create-extract-collection [db table]
-    (do (when (not (pg/schema-exists? db extract-schema))
-          (pg/create-schema db extract-schema))
-        (when (not (pg/table-exists? db extract-schema table))
-          (pg/create-table db extract-schema table
-                     [:id "bigserial" :primary :key]
-                     [:valid_from "timestamp without time zone"]
-                     [:valid_to "timestamp without time zone"]
-                     [:tiles "text"]
-                     [:xml "text"]
-                     [:created_on "timestamp without time zone"]
-                     )
-          )))
+        [nil (map #(vector feature-type (:_tiles %) (m/render template % partials)
+                           (:_valid_from %) (:_valid_to %)) features)]))))
 
 (defn- jdbc-insert-extract [db table entries]
    (try (j/with-db-connection [c db]
           (apply (partial j/insert! c (str extract-schema "." table) :transaction? false
-                    [:valid_from :valid_to :tiles :xml :created_on])
-                    entries))  
+                    [:feature_type :valid_from :valid_to :tiles :xml])
+                    entries))
         (catch java.sql.SQLException e (j/print-sql-exception-chain e))))
 
-(defn add-extractset [db extractset version]
+(defn get-or-add-extractset [db extractset version]
+  "return id"
   (let [query (str "select id from " extractset-table " where extractset_name = ? and version = ?")]
     (j/with-db-connection [c db]
-      (let [result (j/query c [query extractset version])] 
+      (let [result (j/query c [query extractset version])]
         (if (empty? result)
-          (j/insert! db extractset-table {:extractset_name extractset :version version :active false})
-          result)))))
+          (do
+            (j/query c [(str "SELECT " extract-schema ".add_extractset(?,?)") extractset (int version)])
+            (get-or-add-extractset db extractset version))
+
+          (:id (first result)))))))
 
 (defn add-extractset-area [db extractset-id tiles]
   (let [query (str "select * from " extractset-area-table " where extractset_id = ? and area_id = ?")]
@@ -79,34 +69,34 @@
         (if (empty? (j/query c [query extractset-id area-id]))
           (j/insert! db extractset-area-table {:extractset_id extractset-id :area_id area-id}))))))
 
-(defn- tiles-from-feature [[tiles & more]]
+(defn- tiles-from-feature [[type tiles & more]]
    tiles)
 
-(defn add-metadata-extract-records [db dataset extract-type version rendered-features]
-   (let [extractset-id (:id (first (add-extractset db (str dataset "_" extract-type) version)))
-         tiles (reduce clojure.set/union (map tiles-from-feature rendered-features))]
+(defn add-metadata-extract-records [db extractset-id rendered-features]
+   (let [tiles (reduce clojure.set/union (map tiles-from-feature rendered-features))]
      (add-extractset-area db extractset-id tiles)))
 
-(defn- tranform-feature-for-db [[tiles xml-feature valid-from valid-to]]
-  [valid-from valid-to (vec tiles) xml-feature nil])
+(defn- tranform-feature-for-db [[feature-type tiles xml-feature valid-from valid-to]]
+  [feature-type valid-from valid-to (vec tiles) xml-feature])
 
 (defn add-extract-records [db dataset feature-type extract-type version rendered-features]
   "Inserts the xml-features and tile-set in an extract schema based on dataset, extract-type, version and feature-type,
    if schema or table doesn't exists it will be created."
-  (let [table (str dataset "_" extract-type "_v" version "_" feature-type)]
+  (let [collection (str dataset "_" extract-type)
+        table (str collection "_v" version)
+        extractset-id (get-or-add-extractset db collection version) ]
     (do
-      (create-extract-collection db table)
       (jdbc-insert-extract db table (map tranform-feature-for-db rendered-features))
-      (add-metadata-extract-records db dataset extract-type version rendered-features))
+      (add-metadata-extract-records db extractset-id rendered-features))
     (count rendered-features)))
 
 (defn fill-extract [dataset collection extract-type extract-version]
   (let [feature-type collection
         features (timeline/all (config/timeline) dataset collection)
-        [error features-for-extract] (features-for-extract dataset 
-                                                           feature-type 
-                                                           extract-type 
-                                                           features 
+        [error features-for-extract] (features-for-extract dataset
+                                                           feature-type
+                                                           extract-type
+                                                           features
                                                            "src/main/resources/pdok/featured/templates")]
     (if (nil? error)
       (if (nil? features-for-extract)
