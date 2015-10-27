0f724e57bcea649787ec91b8a8a7a034d1400084
Raymond Kroon <raymond.kroon@kadaster.nl>
2015-10-27 14:38:18 +0100

    fix: new gml311 version with less checks

--- project.clj
+++ project.clj
@@ -60,7 +60,7 @@
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-json "0.3.1"]
                  [xalan/xalan "2.7.2"]
-                 [nl.pdok/gml311-jts "0.0.2"]]
+                 [nl.pdok/gml311-jts "0.0.3"]]
   :plugins [[lein-ring "0.9.6" ]
             [lein-filegen "0.1.0-SNAPSHOT"]]
   :ring {:handler pdok.featured.api/app
