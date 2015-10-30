5758d4c7427e43953234aba4e11979aac0876402
Raymond Kroon <raymond.kroon@kadaster.nl>
2015-10-30 17:37:33 +0100

    fix: better build scripts with env

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env sh
 export http_no_proxy="*.so.kadaster.nl"
 lein build && ./make-dar.sh
--- make-dar.sh
+++ make-dar.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 cd target
 zip featured.dar deployit-manifest.xml uberjar/featured.war
