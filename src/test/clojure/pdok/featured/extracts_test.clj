4ee59894db0d00ebe7753c2678d53c155cb88f93
Niek Hoogma <niek.hoogma@kadaster.nl>
2016-01-08 11:07:44 +0100

    PDOK-2292: Fix -elem-at test. Work was done on branch but in the meantime the json-reader on master was altered.

--- src/test/clojure/pdok/featured/extracts_test.clj
+++ src/test/clojure/pdok/featured/extracts_test.clj
@@ -55,7 +55,7 @@
 
 ;; The -elem-at- construction can be used in mustache templates to get an element from a specific position in a list. For instance elem-at-1 returns the second element in list.
 ;; In the example template 3 elements are used. In the example input data there are two elements in "nummeraanduidingreeks" present. In the output only these two elements can be found.
-(def elem-at-inputdata  (file-to-features (io/resource "templates/test/elemat/bgt_pand.json") "bgtmutatie" ))
+(def elem-at-inputdata  (second (file-to-features (io/resource "templates/test/elemat/bgt_pand.json") "bgtmutatie" )))
 (def test-indexed-section (slurp (io/resource "templates/test/elemat/indexedsection.mustache")))
 (def elem-at-expectedoutput "<imgeo-s:Pand><elem1>1111111111111111<hoek1>A</hoek1><hoek2>B</hoek2></elem1><elem2>9999999999999999<hoek3>C</hoek3></elem2></imgeo-s:Pand>")
 (deftest test-elem-at
