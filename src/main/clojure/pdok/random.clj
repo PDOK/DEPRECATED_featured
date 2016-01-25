056812cd2420765bf4f4bcce3a1816a7f7fae9b2
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-01-25 16:28:32 +0100

    fix: random.clj reflection warnings

--- src/main/clojure/pdok/random.clj
+++ src/main/clojure/pdok/random.clj
@@ -1,6 +1,7 @@
 (ns pdok.random
-  (:import com.fasterxml.uuid.Generators))
+  (:import [com.fasterxml.uuid Generators]
+           [com.fasterxml.uuid.impl TimeBasedGenerator]))
 
 (def ^:private UUIDGenerator (Generators/timeBasedGenerator))
 
-(defn ordered-UUID [] (locking UUIDGenerator (.generate UUIDGenerator)))
+(defn ordered-UUID [] (locking UUIDGenerator (.generate ^TimeBasedGenerator UUIDGenerator)))
