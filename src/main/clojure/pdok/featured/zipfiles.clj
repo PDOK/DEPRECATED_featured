4ec82b7adab6950902ddadeb96f068cb6cabca55
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-01-25 18:28:57 +0100

    fix: zipfiles.clj reflection warnings

--- src/main/clojure/pdok/featured/zipfiles.clj
+++ src/main/clojure/pdok/featured/zipfiles.clj
@@ -2,15 +2,15 @@
   (:require  [clojure.tools.logging :as log])
   (:import [java.util.zip ZipInputStream]))
 
-(defn zip-as-input [inputstream] 
+(defn zip-as-input [inputstream]
   (let [zip (ZipInputStream. inputstream)
         ze (.getNextEntry zip)]
-  (if (nil? ze) 
+  (if (nil? ze)
     (throw (Exception. "No entries in zipfile."))
     zip)))
 
 
-(defn close-zip [zip]
-    (do (log/info "close zip")
-      	(.closeEntry zip)
-        (.close zip)))           
+(defn close-zip [^ZipInputStream zip]
+  (log/info "close zip")
+  (.closeEntry zip)
+  (.close zip))
