445f7b249c8d52d32a847113807df45fe3b044c0
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-06-28 16:37:44 +0200

    refac: loop style proc for previous_version update

--- data_migrations/add_current_version.sql
+++ data_migrations/add_current_version.sql
@@ -1,7 +1,35 @@
 ALTER TABLE feature_stream
     ADD COLUMN previous_version UUID;
 
+
+-- set previous version UPDATE STYLE
 UPDATE feature_stream as fs SET previous_version =
 (SELECT prev_version FROM
   (SELECT id, lag(version) OVER (PARTITION BY collection, feature_id ORDER BY id ASC) prev_version
    FROM feature_stream as src where src.collection = fs.collection and src.feature_id = fs.feature_id) AS prev where prev.id = fs.id);
+
+-- set previous version LOOP STYLE
+DO
+$$
+DECLARE
+  ref REFCURSOR;
+  row RECORD;
+  prev_version UUID;
+BEGIN
+  OPEN ref FOR EXECUTE 'SELECT * FROM feature_stream';
+  LOOP
+    FETCH NEXT FROM ref INTO row;
+    EXIT WHEN row is null;
+
+    SELECT version INTO prev_version FROM (
+                                            SELECT src.id, lag(version) OVER (PARTITION BY collection, feature_id ORDER BY id ASC) AS version
+                                            FROM feature_stream as src
+                                            WHERE src.collection = row.collection
+                                                  AND src.feature_id = row.feature_id
+                                            ORDER BY src.id) AS r
+    WHERE r.id = row.id;
+
+    UPDATE feature_stream SET previous_version = prev_version WHERE CURRENT OF ref;
+  END LOOP;
+END;
+$$;
