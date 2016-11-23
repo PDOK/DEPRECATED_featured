870211c785626013a77dcff41a0765e3c3c6abc5
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-11-23 17:44:42 +0100

    travis: docker image

---
+++ .travis/create-docker-snapshot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -ev
+export VERSION=$(printf $(cat VERSION))
+
+$lein with-profile +web-jar build
+cp target/featured-$VERSION-web.jar target/featured-web.jar
+
+docker build --build-arg version=$VERSION . -t pdok/featured:$TRAVIS_COMMIT
+docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+docker push pdok/featured:$TRAVIS_COMMIT
