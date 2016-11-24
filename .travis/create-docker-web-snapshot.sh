d3d571c2a1f9efd0c6ddb242cc762eff740944bd
Raymond Kroon <raymond.kroon@kadaster.nl>
2016-11-24 16:42:46 +0100

    chore: docker images for cli and web

--- .travis/create-docker-web-snapshot.sh
+++ .travis/create-docker-web-snapshot.sh
@@ -6,6 +6,6 @@ export VERSION=$(printf $(cat VERSION))
 $lein with-profile +web-jar build
 cp target/featured-$VERSION-web.jar target/featured-web.jar
 
-docker build --build-arg version=$VERSION . -t pdok/featured:$VERSION
+docker build --build-arg version=$VERSION . -f docker-web/Dockerfile -t pdok/featured-web:$TRAVIS_COMMIT
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker push pdok/featured:$VERSION
+docker push pdok/featured-web:$TRAVIS_COMMIT
