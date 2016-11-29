5380ee0234aa9eee15d5baf223fa6ce5f958b18a
jurgents <jurgen.testroet@kadaster.nl>
2016-11-29 10:20:22 +0100

    Use released version of featured-shared with GeometryAttribute
    [PDOK-3512]

--- project.clj
+++ project.clj
@@ -41,7 +41,7 @@
                  [prismatic/schema "0.4.3"]
                  [ring/ring-defaults "0.1.5"]
                  [ring/ring-json "0.3.1"]
-                 [nl.pdok/featured-shared "1.0-SNAPSHOT"]]
+                 [nl.pdok/featured-shared "1.0-rc4"]]
   :plugins [[lein-ring "0.9.6" ]
             [lein-filegen "0.1.0-SNAPSHOT"]]
   :ring {:handler pdok.featured.api/app
