f6d4aa7feef540c552b2eba8d8ca83f241bfd5ff
Jurgen te Stroet <jurgen.testroet@gmail.com>
2016-12-15 17:22:31 +0100

    Bug fix: create GeometryAttribute from wkt (with SRID)
    [PDOK-3512 bug fix]

--- src/test/clojure/pdok/featured/json_reader_test.clj
+++ src/test/clojure/pdok/featured/json_reader_test.clj
@@ -1,7 +1,8 @@
 (ns pdok.featured.json-reader-test
   (:require [pdok.featured.json-reader :refer :all]
             [clj-time.core :as time]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all])
+  (:import (pdok.featured GeometryAttribute)))
 
 (deftest date-time-with-timezone-to-local
   (let [datetimestring "2016-01-01T12:00:00.000+02:00"
@@ -14,3 +15,14 @@
         required (time/local-date-time 2016 1 1 12 0 0 0)
         parsed (parse-time datetimestring)]
     (is (= required parsed))))
+
+(def ^{:private true} wkt-multipolygon "MULTIPOLYGON (((114326.97399999946 488016.4820000008, 114317.15399999917 488016.37999999896, 114316.77800000086 488052.61300000176, 114326.59800000116 488052.7170000002, 114326.97399999946 488016.4820000008)), ((114326.44099999964 488067.7740000002, 114316.63500000164 488067.6700000018, 114316.24399999902 488103.8830000013, 114326.06599999964 488103.9849999994, 114326.44099999964 488067.7740000002)))")
+
+(deftest srid-geometry-atrribute
+  (let [geometry-map {"srid" 28992 "wkt" wkt-multipolygon "type" "wkt"}
+        ga (create-geometry-atrribute geometry-map)]
+    (is (= 28992 (.getSrid ga)))))
+
+(deftest invalid-srid-geometry-atrribute
+  (let [geometry-map {"srid" "invalid srid" "wkt" wkt-multipolygon "type" "wkt"}]
+    (is (thrown? java.lang.NumberFormatException (create-geometry-atrribute geometry-map)))))
