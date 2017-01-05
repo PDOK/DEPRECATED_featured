2431d191783808a9e8abb373e651684d2e951929
Jurgen te Stroet <jurgen.testroet@gmail.com>
2017-01-05 14:22:05 +0100

    Fix: get only first parameter when using function ~#geometry
    [PDOK-3691]

--- src/main/clojure/pdok/featured/json_reader.clj
+++ src/main/clojure/pdok/featured/json_reader.clj
@@ -102,6 +102,7 @@
     (Integer. srid)))
 
 (defn create-geometry-atrribute [geometry]
+  (println "Geometry: " geometry)
   (if-let [type (get geometry "type")]
     (GeometryAttribute. type (get geometry type) (get-valid-srid geometry))))
 
@@ -113,7 +114,7 @@
       "~#int"     (if params (int (first params)) (nilled java.lang.Integer))
       "~#boolean" (if params (boolean (first params)) (nilled java.lang.Boolean))
       "~#double"  (if params (double (first params)) (nilled java.lang.Double))
-      "~#geometry" (if params (create-geometry-atrribute params) (nilled pdok.featured.GeometryAttribute))
+      "~#geometry" (if params (create-geometry-atrribute (first params)) (nilled pdok.featured.GeometryAttribute))
       element ; never fail just return element
       ))
   )
