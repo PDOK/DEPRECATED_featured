d8a9b600c7bd110988846c9d00487429b8ca4297
Bas Duineveld <Bas.Duineveld@kadaster.nl>
2017-02-21 16:11:01 +0100

    Update release script

--- prepare_release.sh
+++ prepare_release.sh
@@ -9,15 +9,15 @@ read release_version
 
 printf $release_version > VERSION
 git add VERSION
-git commit -q -m "chore: prepare release $release_version"
+git commit -q -m "Prepare release $release_version"
 
-git tag -a release-$(cat VERSION) -m "release $(cat VERSION)"
+git tag -a featured-$(cat VERSION) -m "Release $(cat VERSION)"
 
 printf "New development version? "
 read dev_version
 
 printf $dev_version > VERSION
 git add VERSION
-git commit -q -m "chore: new release cycle $dev_version [ci skip]"
+git commit -q -m "New release cycle $dev_version [ci skip]"
 
 git push --follow-tags
