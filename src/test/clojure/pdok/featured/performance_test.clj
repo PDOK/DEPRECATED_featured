7ea352d03ddb5960dff6770e2dedd10c193c92ae
Bas Duineveld <Bas.Duineveld@kadaster.nl>
2017-07-24 16:30:51 +0200

    Remove vacuum analyze from performance test

--- src/test/clojure/pdok/featured/performance_test.clj
+++ src/test/clojure/pdok/featured/performance_test.clj
@@ -15,11 +15,6 @@
   (j/execute! test-db ["DROP SCHEMA IF EXISTS \"featured_performance_performance-set\" CASCADE"])
   (j/execute! test-db ["DROP SCHEMA IF EXISTS \"performance-set\" CASCADE"]))
 
-(defn vacuum-tables []
-  (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".feature"] :transaction? false)
-  (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".feature_stream"] :transaction? false)
-  (j/execute! test-db ["VACUUM ANALYZE \"featured_performance_performance-set\".timeline_col1"] :transaction? false))
-
 (defn generate-new-features [ids difficult?]
   (with-bindings {#'generator/*difficult-geometry?* difficult?}
     (generator/random-json-feature-stream "performance-set" "col1" (count ids)
@@ -52,8 +47,7 @@
                       (merge {:check-validity-on-delete false} cfg)
                       "performance-set" persistence [timeline])]
       (dorun (processor/consume processor features))
-      (let [statistics (processor/shutdown processor)
-            _ (vacuum-tables)]
+      (let [statistics (processor/shutdown processor)]
       (:statistics statistics)))))
 
 (deftest double-file-test
